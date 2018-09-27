2fe7a8f3037ea613e295aaff5512c2c325fed4e4
LaOwlLol <something.less.than.nate@gmail.com>
2018-09-27 14:28:25 -0500

    debug
    Added debug to TileImageDirectory.

--- src/main/java/fauxpas/collections/TileImageDirectory.java
+++ src/main/java/fauxpas/collections/TileImageDirectory.java
@@ -12,17 +12,28 @@ public class TileImageDirectory {
     private HashMap<String, Image> keytoImage;
 
     private int AssetDimension;
+    boolean debug;
 
     public TileImageDirectory(int assetDimension) {
         this.keytoImage = new HashMap<>();
         this.AssetDimension = assetDimension;
+        this.debug = false;
+    }
+
+    public TileImageDirectory(int assetDimension, boolean debug) {
+        this.keytoImage = new HashMap<>();
+        this.AssetDimension = assetDimension;
+        this.debug = debug;
     }
 
     public void addTile(Tile tile, String url) {
         this.keytoImage.put(TileToKey(tile), new Image(url));
     }
 
-    public Optional<Image> getTileAsset(Tile tile) {
+    public Optional<Image> getTileAsset(Tile tile) {\
+        if (this.debug) {
+            System.out.println("Fetching '" + TileToKey(tile) + "' : " + keytoImage.containsKey(TileToKey(tile)));
+        }
         return Optional.ofNullable(this.keytoImage.get(TileToKey(tile)));
     }
 
