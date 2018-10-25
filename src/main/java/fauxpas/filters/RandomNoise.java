6899ce3cf246fecb9b5e98272ab66acabcef90a0
LaOwlLol <something.less.than.nate@gmail.com>
2018-10-24 22:57:12 -0500

    try sine system time instead of pure random noise.

--- src/main/java/fauxpas/filters/RandomNoise.java
+++ src/main/java/fauxpas/filters/RandomNoise.java
@@ -13,10 +13,10 @@ public class RandomNoise implements Filter {
 
         WritableImage buffer = new WritableImage((int)image.getWidth(), (int)image.getHeight());
         PixelWriter bufferWriter = buffer.getPixelWriter();
-        Random random = new Random(System.currentTimeMillis());
+        //Random random = new Random(System.currentTimeMillis());
         for (int j = 0; j < image.getHeight(); ++j) {
             for (int i = 0; i < image.getWidth(); ++i) {
-                double color = Math.abs(Math.sin(random.nextGaussian()));
+                double color = Math.abs(Math.sin(System.currentTimeMillis()));
                 bufferWriter.setColor(i, j, new Color(color, color, color, 1.0));
             }
         }
