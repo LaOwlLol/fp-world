995c9b16013e76d8b56bef0f3ef38a2e31930b75
LaOwlLol <something.less.than.nate@gmail.com>
2018-10-24 22:53:36 -0500

    added new constructor to depthMap which takes dimension (width/height) to new random noise height map.

--- src/main/java/fauxpas/entities/DepthMap.java
+++ src/main/java/fauxpas/entities/DepthMap.java
@@ -1,13 +1,21 @@
 package fauxpas.entities;
 
 import fauxpas.filters.Filter;
+import fauxpas.filters.RandomNoise;
 import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 
 import java.nio.file.Paths;
 
 public class DepthMap {
 
     private Image depthImage;
+
+    public DepthMap(int width, int height) {
+        this.depthImage = new WritableImage(width, height);
+        RandomNoise noiseGen = new RandomNoise();
+        this.applyFilter(noiseGen);
+    }
 
     public DepthMap(Image depthImage) {
         this.depthImage = depthImage;
